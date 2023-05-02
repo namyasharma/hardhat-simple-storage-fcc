65b8523eb99ddd1dee26cfee134510b02122b7bf
namya <namyas@gmail.com>
2023-05-02 18:50:18 +0530

    Created first hardhat project

---
+++ scripts/deploy.js
@@ -0,0 +1,52 @@
+// imports
+const { ethers, run, network } = require("hardhat")
+
+//async
+async function main() {
+    const SimpleStotageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Deploying contract...")
+    const simpleStorage = await SimpleStotageFactory.deploy()
+    await simpleStorage.deployed()
+    console.log(`Deployed contract to : ${simpleStorage.address}`)
+    //what happens when we deploy our code to the hardhat network?
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current value is ${currentValue}`)
+
+    //Update current value
+    const transactionResponse = await simpleStorage.store(10)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated value is ${updatedValue}`)
+
+    const addPersonResponse = await simpleStorage.addPerson("Namya", 7)
+    await addPersonResponse.wait(1)
+    const updatedList = await simpleStorage.getValueAtMapping("Namya")
+    console.log(`Updated list is : ${updatedList}`)
+}
+
+//verify
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified")
+        } else console.log(e)
+    }
+}
+
+//main
+main()
+    .then(() => process.exit(0))
+    .catch((error) => console.error(error))
---
+++ test/test-deploy.js
@@ -0,0 +1,33 @@
+const { ethers } = require("hardhat")
+const { expect, assert } = require("chai")
+
+describe("SimpleStorage", function () {
+    let simpleStotageFactory, simpleStorage
+
+    beforeEach(async function () {
+        simpleStotageFactory = await ethers.getContractFactory("SimpleStorage")
+        simpleStorage = await simpleStotageFactory.deploy()
+    })
+
+    it("Should start with a favourite number of 0", async function () {
+        const currentValue = await simpleStorage.retrieve()
+        const expectedValue = 0
+        assert.equal(currentValue.toString(), expectedValue)
+    })
+
+    it("Should update when we call store", async function () {
+        const expectedValue = await simpleStorage.retrieve()
+        const transactionResponse = await simpleStorage.store(10)
+        await transactionResponse.wait(1)
+        const currentValue = await simpleStorage.retrieve()
+        assert.notEqual(currentValue.toString(), expectedValue)
+    })
+
+    it("Should add a person to the mapping when we call addPerson", async function () {
+        const expectedValue = await simpleStorage.getValueAtMapping("Namya")
+        const transactionResponse = await simpleStorage.addPerson("Namya", 10)
+        await transactionResponse.wait(1)
+        const currentValue = await simpleStorage.getValueAtMapping("Namya")
+        assert.notEqual(currentValue, expectedValue)
+    })
+})
